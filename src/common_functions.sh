ea1e10b775e0f26c37b602f3d59d55c7db83ef7f
Rawiri Blundell <rawiri.blundell@gmail.com>
2020-09-06 23:23:00 +1200

    Added 'rev' command

--- src/common_functions.sh
+++ src/common_functions.sh
@@ -5,4 +5,10 @@ var_is_unset() { [ -z "${1+x}" ]; }                             # unset
 var_is_empty() { [ "${1+x}" = "x" ] && [ "${#1}" -eq "0" ]; }   # set and null
 var_is_blank() { var_is_unset "${1}" || var_is_empty "${1}"; }  # unset, or set and null
 
-
+die() {
+  case "${-}" in
+    (*i*) printf -- '\e[38;5;9m%s\e[m\n' "${0}: ${*}" >&2 ;;
+    (*)   printf -- '%s' "${0}: ${*}" >&2 ;;
+  esac
+  exit 1
+}
